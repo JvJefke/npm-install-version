ac09b8c55b6c62be136c7cb8fd29314cb1738cbb
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-10-05 10:59:08 +0200

    Fix tests

--- src/test/install.js
+++ src/test/install.js
@@ -7,7 +7,7 @@ const { clean } = require('./test-util.js');
 test('niv.install normal', t => {
   clean();
   niv.install('csjs@1.0.0');
-  const packageJson = fs.readFileSync('node_modules/csjs@1.0.0/package.json');
+  const packageJson = fs.readFileSync('niv_modules/csjs@1.0.0/package.json');
   t.equal(JSON.parse(packageJson).version, '1.0.0');
   t.end();
 });
@@ -15,7 +15,7 @@ test('niv.install normal', t => {
 test('niv.install remote', t => {
   clean();
   niv.install('scott113341/csjs#extract-extends-performance');
-  const packageJson = fs.readFileSync('node_modules/scott113341-csjs#extract-extends-performance/package.json');
+  const packageJson = fs.readFileSync('niv_modules/scott113341-csjs#extract-extends-performance/package.json');
   t.equal(JSON.parse(packageJson).version, '1.0.4');
   t.end();
 });
@@ -23,7 +23,7 @@ test('niv.install remote', t => {
 test('niv.install scoped', t => {
   clean();
   niv.install('@scott113341/niv-scoped-test@1.0.0');
-  const packageJson = fs.readFileSync('node_modules/@scott113341-niv-scoped-test@1.0.0/package.json');
+  const packageJson = fs.readFileSync('niv_modules/@scott113341-niv-scoped-test@1.0.0/package.json');
   t.equal(JSON.parse(packageJson).version, '1.0.0');
   t.end();
 });
@@ -31,7 +31,7 @@ test('niv.install scoped', t => {
 test('niv.install w/ dependencies', t => {
   clean();
   niv.install('push-dir@0.2.2');
-  const packageJson = fs.readFileSync('node_modules/push-dir@0.2.2/package.json');
+  const packageJson = fs.readFileSync('niv_modules/push-dir@0.2.2/package.json');
   t.equal(JSON.parse(packageJson).version, '0.2.2');
   t.end();
 });
@@ -39,7 +39,7 @@ test('niv.install w/ dependencies', t => {
 test('niv.install w/ destination', t => {
   clean();
   niv.install('csjs@1.0.0', { destination: 'csjs@yolo' });
-  const packageJson = fs.readFileSync('node_modules/csjs@yolo/package.json');
+  const packageJson = fs.readFileSync('niv_modules/csjs@yolo/package.json');
   t.equal(JSON.parse(packageJson).version, '1.0.0');
   t.end();
 });
@@ -47,11 +47,11 @@ test('niv.install w/ destination', t => {
 test('niv.install w/o overwrite', t => {
   clean();
   niv.install('csjs@1.0.0', { destination: 'csjs@yolo' });
-  const packageJson1 = fs.readFileSync('node_modules/csjs@yolo/package.json');
+  const packageJson1 = fs.readFileSync('niv_modules/csjs@yolo/package.json');
   t.equal(JSON.parse(packageJson1).version, '1.0.0');
 
   niv.install('csjs@1.0.1', { destination: 'csjs@yolo' });
-  const packageJson2 = fs.readFileSync('node_modules/csjs@yolo/package.json');
+  const packageJson2 = fs.readFileSync('niv_modules/csjs@yolo/package.json');
   t.equal(JSON.parse(packageJson2).version, '1.0.0');
 
   t.end();
@@ -60,11 +60,11 @@ test('niv.install w/o overwrite', t => {
 test('niv.install w/ overwrite', t => {
   clean();
   niv.install('csjs@1.0.0', { destination: 'csjs@yolo' });
-  const packageJson1 = fs.readFileSync('node_modules/csjs@yolo/package.json');
+  const packageJson1 = fs.readFileSync('niv_modules/csjs@yolo/package.json');
   t.equal(JSON.parse(packageJson1).version, '1.0.0');
 
   niv.install('csjs@1.0.1', { destination: 'csjs@yolo', overwrite: true });
-  const packageJson2 = fs.readFileSync('node_modules/csjs@yolo/package.json');
+  const packageJson2 = fs.readFileSync('niv_modules/csjs@yolo/package.json');
   t.equal(JSON.parse(packageJson2).version, '1.0.1');
 
   t.end();
--- src/test/test-util.js
+++ src/test/test-util.js
@@ -1,7 +1,7 @@
 const shelljs = require('shelljs');
 
 function clean () {
-  shelljs.rm('-rf', 'node_modules/csjs*', 'node_modules/@scott113341*', 'node_modules/scott113341*', 'node_modules/push-dir*');
+  shelljs.rm('-rf', 'niv_modules/csjs*', 'niv_modules/@scott113341*', 'niv_modules/scott113341*', 'niv_modules/push-dir*');
 }
 
 module.exports = {
