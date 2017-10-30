722144a1421fd20105c0dff455011b85747313ff
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-10-30 21:40:26 +0100

    remove logged old code

--- src/install.js
+++ src/install.js
@@ -74,57 +74,6 @@ function install (npmPackage, options = {}) {
         throw Error(`Error installing ${npmPackage}`);
       }
     );
-
-
-  // util.tryCatchOptimizer(function() {
-  //    // make temp install dir
-  //    shelljs.rm('-rf', TEMP);
-  //    shelljs.mkdir('-p', path.join(TEMP, 'node_modules'));
-
-  //    // copy local .npmrc file if exists
-  //    const npmrcFile = path.join(CWD, '.npmrc');
-  //    if (shelljs.test('-f', npmrcFile)) {
-  //      shelljs.cp(npmrcFile, TEMP);
-  //    }
-
-  //    // install package to temp dir
-  //    const installOptions = {
-  //      cwd: TEMP,
-  //      stdio: [null, null, null]
-  //    };
-  //    const command = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-  //    childProcess.spawnSync(command, ['install', npmPackage], installOptions);
-
-  //    // get real package name
-  //    const packageName = util.getPackageName(npmPackage);
-
-  //    // move deps inside package
-  //    shelljs.mkdir('-p', path.join(TEMP, 'node_modules', packageName, 'node_modules'));
-  //    shelljs.ls(path.join(TEMP, 'node_modules'))
-  //      .forEach(dep => {
-  //        if (dep === packageName) return;
-  //        const from = path.join(TEMP, 'node_modules', dep).toString();
-  //        const to = path.join(TEMP, 'node_modules', packageName, 'node_modules', dep).toString();
-  //        shelljs.mv(from, to);
-  //      });
-
-  //    // copy to niv_modules/
-  //    shelljs.rm('-rf', destinationPath);
-  //    shelljs.mv(path.join(TEMP, 'node_modules', packageName), destinationPath);
-
-  //    log(`Installed ${npmPackage} to ${destinationPath}`);
-  // }, function onError(error) {
-  //   errored = true;
-  //   console.error(`Error installing ${npmPackage}`);
-  //   console.error(err.toString());
-  // }, function final() {
-  //   // clean up temp install dir
-  //   shelljs.rm('-rf', TEMP);
-
-  //   if (errored) {
-  //     throw Error(`Error installing ${npmPackage}`);
-  //   }
-  // });
 }
 
 module.exports = install;
